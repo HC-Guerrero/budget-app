58f350d9c1be750079b9fa95bdbd35d8455dbef5
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-07-22 11:42:08 -0300

    port changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = require('./app');
 const cors = require('cors');
 require('dotenv').config();
-const PORT = process.env.OUR_PORT || 3008;
+const PORT = process.env.PORT || 3008;
 
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
